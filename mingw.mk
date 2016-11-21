1bde17c35c009ffba29c2a6443849a4a7a9ef85f
Michel Mooij <michel.mooij7@gmail.com>
2016-11-21 15:39:42 +0100

    added missing mingw.mk.

---
+++ mingw.mk
@@ -0,0 +1,22 @@
+PREFIX  	?= .
+CFLAGS  	:= -fPIC -O3 -g -Wall -Werror -I$(PREFIX)/include -I.
+LINKFLAGS 	:= -L$(PREFIX)/bin -ljansson-4
+MAJOR   	:= 1
+NAME    	:= jsonrpc
+VERSION 	:= $(MAJOR)
+TARGET  	:= lib$(NAME)-$(VERSION)
+
+all: clean
+	$(CC) -c jsonrpc.c $(CFLAGS)
+	$(CC) -c jsonrpc_notify.c $(CFLAGS)
+	$(CC) -shared -o $(TARGET).dll *.o -Wl,--out-implib,$(TARGET).a $(LINKFLAGS)
+
+clean:
+	$(RM) *.o *.a *.dll *.la *.so*
+
+install: all
+	mkdir -p $(PREFIX)/bin
+	mkdir -p $(PREFIX)/include
+	cp $(TARGET).dll $(PREFIX)/bin
+	cp *.h $(PREFIX)/include
+
